d67cd26e04d83a66625e51ec4137146b0678c575
John <john.mandia@gmail.com>
2021-06-21 19:39:16 +0100

    path update

--- client-sdk/common/connect.js
+++ client-sdk/common/connect.js
@@ -2,7 +2,7 @@ const SDK_PREFIX = "ORG-SDK-";
 
 export async function connect(version, payload) {
   await window.fin.System.launchManifest(
-    "fins://johnman.github.io/openfin-headless-app-template/config/app.platform.fin.json?$$version=" +
+    "fins://johnman.github.io/openfin-headless-app-template/config/headless.app.fin.json?$$version=" +
       version
   );
   let channel = await window.fin.InterApplicationBus.Channel.connect(
